8bee8b6ec4d4d16418a0cbb6a1aced162554e639
johnnyrogers <johnnyrogers.demadara@protonmail.com>
2020-11-26 08:10:12 +0800

    no message

---
+++ src/lozadagroupcompany/Main.java
@@ -0,0 +1,22 @@
+package lozadagroupcompany;
+
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        FrameMain window = new FrameMain();
+        window.setVisible(true);
+    }
+
+}
